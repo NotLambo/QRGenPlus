03972fa8d09aec042bd27a1ad237e16167e285f2
Lambo <100071045+NotLambo@users.noreply.github.com>
2023-12-01 22:49:05 -0600

    Update NavigationBar.jsx
    Added more padding for the y-axis

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,7 +4,7 @@ import NavigationMenu from "./NavigationMenu";
 function NavigationBar() {
   const [ showMenu, setShowMenu ] = useState(false);
   return (
-    <nav className='sticky top-0 z-50 flex flex-wrap items-center justify-between w-full h-auto p-2 my-1 bg-backgroundColor'>
+    <nav className='sticky top-0 z-50 flex flex-wrap items-center justify-between w-full h-auto p-6 bg-backgroundColor'>
       <img
         className='w-auto h-5 transition-all'
         src='./qrgenplus-width-logo_sm.webp'
