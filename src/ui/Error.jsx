6644fefd8a6e6168b54dc8e5d89f5b16494b3b59
Lambo <100071045+NotLambo@users.noreply.github.com>
2023-12-08 23:42:33 -0600

    Delete src directory

--- src/ui/Error.jsx
+++
@@ -1,9 +0,0 @@
-function Error({ children }) {
-    return (
-        <section className='p-2 m-auto my-4 bg-red-700 rounded-lg w-fit'>
-            <h1 className="text-center text-gray-200">{children}</h1>
-        </section>
-    )
-}
-
-export default Error
